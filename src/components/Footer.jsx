7254b9037da42e559e42ad97cd0586383bed7acf
Anurag Saraswat <anu01saraswat@gmail.com>
2022-01-14 21:17:00 +0530

    First Commit on main branch

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { FaGithub } from "react-icons/fa"
+
+const Footer = () => {
+    return (
+        <footer>
+            Developed by Anurag Saraswat 
+            <a href="/"><FaGithub /></a>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div className="navbar">
+            <a className="logo" href="/"><h1>Music Uploader</h1></a>
+        </div>
+    )
+}
+
+export default Header
